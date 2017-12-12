dd661a46c8024bea4d2961a27722ececb89df95d
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-12-12 14:10:44 -0500

    predictingAge

--- main.R
+++ main.R
@@ -1,4 +1,4 @@
-# Feature Engineering submission
+# predicting Age submission
 library(rpart)
 
 train <- read.csv('./train.csv')
@@ -21,6 +21,8 @@ total$SurName <- sapply(total$Name, FUN=function(x) { strsplit(x, split='[,.]')[
 total$Family <- factor(paste(total$SurName, as.character(total$FamilySize), sep='_'))
 total$Family[total$FamilySize == 1 ] <- NA
 
+age <- rpart(Age ~ Survived + Title + Pclass + Sex + Fare + Embarked, data=total[!is.na(total$Age),], method='anova')
+total$Age[is.na(total$Age)] <- predict(age, total[is.na(total$Age),])
 
 train <- total[1:891,]
 test <- total[892:1309,]
@@ -28,4 +30,4 @@ test <- total[892:1309,]
 fit <- rpart(Survived ~ Family + Title + Pclass + Sex + Age + SibSp + Parch + FamilySize + Fare + Embarked, data=train, method="class")
 res <- predict(fit, test, type="class")
 submission <- data.frame(PassengerId = test$PassengerId, Survived = as.integer(res) - 1)
-write.csv(submission, 'featureEngineering.csv', row.names = FALSE)
+write.csv(submission, 'predictingAge.csv', row.names = FALSE)
