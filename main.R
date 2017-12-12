6ef1546307e773a04d237b566e33826fb303d3e5
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-12-12 13:40:58 -0500

    featureEngineering

--- main.R
+++ main.R
@@ -1,13 +1,31 @@
-# 1° Descision Tree submission
+# Feature Engineering submission
 library(rpart)
 
 train <- read.csv('./train.csv')
 test <- read.csv('./test.csv')
+test$Survived <- NA
+total <- rbind(train, test)
 
-fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train,method="class")
+total$FamilySize <- total$SibSp + total$Parch + 1
 
+total$Name <- as.character(total$Name)
+
+total$Title <- sapply(total$Name, FUN=function(x) { strsplit(x, split='[,.]')[[1]][2]})
+total$Title <- sub(' ', '', total$Title)
+total$Title[total$Title %in% c('Mlle', 'Mme')] <- 'Miss'
+total$Title[total$Title %in% c('Don', 'Dona', 'Lady', 'Sir', 'Jonkheer', 'the Countess')] <- 'Knighthoods'
+total$Title[total$Title %in% c('Capt', 'Col', 'Major')] <- 'Military'
+total$Title <- factor(total$Title)
+
+total$SurName <- sapply(total$Name, FUN=function(x) { strsplit(x, split='[,.]')[[1]][1]})
+total$Family <- factor(paste(total$SurName, as.character(total$FamilySize), sep='_'))
+total$Family[total$FamilySize == 1 ] <- NA
+
+
+train <- total[1:891,]
+test <- total[892:1309,]
+
+fit <- rpart(Survived ~ Family + Title + Pclass + Sex + Age + SibSp + Parch + FamilySize + Fare + Embarked, data=train, method="class")
 res <- predict(fit, test, type="class")
-
 submission <- data.frame(PassengerId = test$PassengerId, Survived = as.integer(res) - 1)
-
-write.csv(submission, 'firstOrderTree.csv', row.names = FALSE)
+write.csv(submission, 'featureEngineering.csv', row.names = FALSE)
