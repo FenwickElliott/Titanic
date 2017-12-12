3939f401cb47de68b709f40b7dee22e8e2e0b87f
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-12-12 14:27:59 -0500

    aloneFactor

--- main.R
+++ main.R
@@ -1,12 +1,10 @@
-# predicting Age submission
+# aloneFactor submission
 library(rpart)
 
 train <- read.csv('./train.csv')
 test <- read.csv('./test.csv')
 test$Survived <- NA
 total <- rbind(train, test)
-
-total$FamilySize <- total$SibSp + total$Parch + 1
 
 total$Name <- as.character(total$Name)
 
@@ -17,9 +15,11 @@ total$Title[total$Title %in% c('Don', 'Dona', 'Lady', 'Sir', 'Jonkheer', 'the Co
 total$Title[total$Title %in% c('Capt', 'Col', 'Major')] <- 'Military'
 total$Title <- factor(total$Title)
 
+total$FamilySize <- total$SibSp + total$Parch + 1
 total$SurName <- sapply(total$Name, FUN=function(x) { strsplit(x, split='[,.]')[[1]][1]})
-total$Family <- factor(paste(total$SurName, as.character(total$FamilySize), sep='_'))
-total$Family[total$FamilySize == 1 ] <- NA
+total$Family <- paste(total$SurName, as.character(total$FamilySize), sep='_')
+total$Family[total$FamilySize == 1 ] <- 'alone'
+total$Family <- factor(total$Family)
 
 age <- rpart(Age ~ Survived + Title + Pclass + Sex + Fare + Embarked, data=total[!is.na(total$Age),], method='anova')
 total$Age[is.na(total$Age)] <- predict(age, total[is.na(total$Age),])
@@ -30,4 +30,4 @@ test <- total[892:1309,]
 fit <- rpart(Survived ~ Family + Title + Pclass + Sex + Age + SibSp + Parch + FamilySize + Fare + Embarked, data=train, method="class")
 res <- predict(fit, test, type="class")
 submission <- data.frame(PassengerId = test$PassengerId, Survived = as.integer(res) - 1)
-write.csv(submission, 'predictingAge.csv', row.names = FALSE)
+write.csv(submission, 'aloneFactor.csv', row.names = FALSE)
