2392959ab2a9839d109bfd0f73e44f13fea7843b
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-12-12 09:55:11 -0500

    Gender submission

--- main.R
+++ main.R
@@ -1,4 +1,15 @@
+# Gender submission
+
 train <- read.csv('./train.csv')
 test <- read.csv('./test.csv')
 
-str(train)
+# prop.table(table(train$Sex, train$Survived))
+
+test$Survived <- 0
+test$Survived[test$Sex == 'female'] <- 1
+
+summary(test$Sex)
+summary(test$Survived)
+
+res <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(res, 'gender.csv', row.names = FALSE)
